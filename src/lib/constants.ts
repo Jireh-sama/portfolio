a95c692e6f66551991da6c9a46e11bdba106eddc
Jireh <jirehtumbagahan@gmail.com>
2025-10-08 19:58:37 +0800

    feat: add types and constants file

---
+++ src/lib/constants.ts
@@ -0,0 +1,36 @@
+import type { EducationItemI } from "./types";
+
+export const educationData: EducationItemI[] = [
+  {
+    degree: "Bachelor of Science",
+    field: "Information Technology",
+    institution: "Cavite State University - Bacoor Campus",
+    location: "Philippines, Cavite",
+    period: "2021 — 2025",
+    description:
+      "Focused on Object-Oriented Programming, Data Structures, and Human–Computer Interaction, with interests in Machine Learning applications.",
+    achievements: ["Cum Laude"],
+  },
+  {
+    degree: "Bachelor of Science",
+    field: "Software Engineering",
+    institution: "AMA Computer College - Las Piñas",
+    location: "Philippines, Las Piñas City",
+    period: "2019 — 2021",
+    description:  "Studied Java development, networking fundamentals, and web technologies with an emphasis on software design and user experience.",
+    achievements: ["With High Honors"],
+  },
+  {
+    degree: "High School Diploma",
+    field: "General Education",
+    institution: "Las Piñas National High School – Almanza",
+    location: "Philippines, Las Piñas City",
+    period: "2015 — 2019",
+  },
+];
+
+export const socialContacts = {
+  gmail: "jirehtumbagahan@gmail.com",
+  linkedIn: "https://linkedin.com/in/jireh-tumbagahan",
+  github: "https://github.com/Jireh-sama",
+}
---
+++ src/lib/types.ts
@@ -0,0 +1,9 @@
+export interface EducationItemI {
+  degree: string
+  field: string
+  institution: string
+  location: string
+  period: string
+  description?: string
+  achievements?: string[]
+}
