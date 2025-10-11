a9d840c132e3bd462b7c9a32294c34e84ed3c496
Jireh <jirehtumbagahan@gmail.com>
2025-10-11 13:32:33 +0800

    feat: add work experience, education section and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
 import FloatingNav from "./components/FloatingNav";
 import ScreenSizeIndicator from "./components/ScreenSizeIndicaator";
 import CTAButtonsSection from "./components/section/CTAButtonsSection";
+import EducationSection from "./components/section/EducationSection";
+import FooterSection from "./components/section/FooterSection";
 import HeroSection from "./components/section/HeroSection";
+import WorkExperienceSection from "./components/section/WorkExperienceSection";
+import SimpleDivider from "./components/SimpleDIvider";
+import {
+  educationData,
+  socialContacts,
+  workExperienceData,
+} from "./lib/constants";
+import useSectionScroll from "./hooks/useSectionScroll";
 
 const App = () => {
+  const { educationSectionRef, handleScrollToEducation, handleScrollToTop } =
+    useSectionScroll();
+
   return (
-    <main className="min-h-[100vh]">
+    <main className="min-h-[100vh]" id="smooth-wrapper">
       <ScreenSizeIndicator />
-      <FloatingNav />
-      <div className="px-12 lg:px-60 space-y-14">
-        <HeroSection />
-        <HeroSection />
+      <FloatingNav
+        scrollToHome={handleScrollToTop}
+        scrollToEducation={handleScrollToEducation}
+      />
+      <div className="max-w-6xl mx-auto px-6 space-y-14" id="smooth-content">
         <HeroSection />
         <CTAButtonsSection />
+        <SimpleDivider label="WORK EXPERIENCE" />
+        <WorkExperienceSection workExperienceList={workExperienceData} />
+        <SimpleDivider label="EDUCATION" />
+        <EducationSection
+          educationList={educationData}
+          ref={educationSectionRef}
+        />
       </div>
+      <FooterSection
+        email={socialContacts.gmail}
+        githubLink={socialContacts.github}
+        linkedInLink={socialContacts.linkedIn}
+      />
     </main>
   );
 };
