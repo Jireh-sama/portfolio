f70b85ab72e1105d537332f8e1c3b0e54cbca1e6
Jireh <jirehtumbagahan@gmail.com>
2025-10-14 20:44:47 +0800

    feat: add custom icon components

---
+++ src/components/icon/FigmaIcon.tsx
@@ -0,0 +1,39 @@
+const FigmaIcon = () => {
+  return (
+    <svg
+      width="288"
+      height="432"
+      viewBox="0 0 288 432"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="size-20 group/figma"
+    >
+      <path
+        d="M144 216C144 176.641 175.907 144.733 215.267 144.733C254.626 144.733 286.534 176.641 286.534 216C286.534 255.36 254.626 287.267 215.267 287.267C175.907 287.267 144 255.36 144 216Z"
+        className="group-hover/figma:fill-[#1ABCFE] transition-colors duration-300 fill-foreground"
+      />
+      <path
+        d="M1.46606 358.534C1.46606 319.175 33.3733 287.267 72.733 287.267H144V358.534C144 397.894 112.093 429.801 72.733 429.801C33.3733 429.801 1.46606 397.894 1.46606 358.534Z"
+        fill="white"
+        className="group-hover/figma:fill-[#0ACF83] transition-colors duration-300 fill-foreground"
+      />
+      <path
+        d="M144 2.19946V144.733H215.267C254.627 144.733 286.534 112.826 286.534 73.4664C286.534 34.1068 254.627 2.19946 215.267 2.19946H144Z"
+        fill="white"
+        className="group-hover/figma:fill-[#FF7262] transition-colors duration-300 fill-foreground"
+      />
+      <path
+        d="M1.46606 73.4664C1.46606 112.826 33.3733 144.733 72.733 144.733H144V2.1994H72.733C33.3733 2.1994 1.46606 34.1067 1.46606 73.4664Z"
+        fill="white"
+        className="group-hover/figma:fill-[#F24E1E] transition-colors duration-300 fill-foreground"
+      />
+      <path
+        d="M1.46606 216C1.46606 255.36 33.3733 287.267 72.733 287.267H144V144.733H72.733C33.3733 144.733 1.46606 176.641 1.46606 216Z"
+        fill="white"
+        className="group-hover/figma:fill-[#A259FF] transition-colors duration-300 fill-foreground"
+      />
+    </svg>
+  );
+};
+
+export default FigmaIcon;
---
+++ src/components/icon/Firebase.tsx
@@ -0,0 +1,31 @@
+const Firebase = () => {
+  return (
+    <svg
+      width="100"
+      height="100"
+      viewBox="0 0 100 100"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className="size-20 group/firebase"
+    >
+      <path
+        d="M16.6667 77.0834L48.4042 17.575C49.073 16.3209 50.8813 16.3438 51.5167 17.6167L62.5001 39.5834L16.6667 77.0834Z"
+        className="group-hover/firebase:fill-[#FF8F00] fill-foreground transition-colors duration-300"
+      />
+      <path
+        d="M16.6667 77.0666L28.2209 5.81872C28.523 3.95413 31.0272 3.54997 31.9022 5.22497L54.1667 47.9L16.6667 77.0666Z"
+        className="fill-foreground group-hover/firebase:fill-[#FFA000] transition-colors duration-300"
+      />
+      <path
+        d="M16.6833 77.0541L17.1 76.727L53.6188 46.8499L43.3188 27.1083L16.6833 77.0541Z"
+        className="fill-foreground group-hover/firebase:fill-[#FF6F00] transition-colors duration-300"
+      />
+      <path
+        d="M16.6667 77.0834L72.2209 23.5146C73.3855 22.3917 75.3292 23.0542 75.5667 24.6542L83.3334 77.0834L52.0834 95.2979C50.8147 96.0104 49.2667 96.0104 47.998 95.2979L16.6667 77.0834Z"
+        className="fill-foreground group-hover/firebase:fill-[#FFC400] transition-colors duration-300"
+      />
+    </svg>
+  );
+};
+
+export default Firebase;
