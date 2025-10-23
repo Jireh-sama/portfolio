4a62920a90a60ab6318a8ab8ac7e4a9193edab25
Jireh <jirehtumbagahan@gmail.com>
2025-10-23 14:35:09 +0800

    feat: add redirect links when clicking buttons

--- src/components/CTAButtons.tsx
+++ src/components/CTAButtons.tsx
@@ -3,19 +3,38 @@ import { LuFileDown, LuGithub, LuLinkedin, LuMail } from "react-icons/lu";
 
 const CTAButtons = () => {
   const socialButtons = [
-    { icon: <LuGithub className="size-5" /> },
-    { icon: <LuLinkedin className="size-5" /> },
-    { icon: <LuMail className="size-5" /> },
+    {
+      icon: (
+        <a href="https://github.com/jireh-sama/" target="_blank">
+          {" "}
+          <LuGithub className="size-5" />
+        </a>
+      ),
+    },
+    {
+      icon: (
+        <a href="https://www.linkedin.com/in/jireh-tumbagahan/" target="_blank">
+          <LuLinkedin className="size-5" />
+        </a>
+      ),
+    },
+    {
+      icon: (
+        <a
+          href="https://mail.google.com/mail/?view=cm&to=jirehtumbagahan@gmail.com.com&su=Let’s Work Together!&body=Hey%20Jireh,%0D%0A%0D%0AI'd%20love%20to%20talk%20about%20a%20project%20or%20opportunity."
+          target="_blank"
+        >
+          <LuMail className="size-5" />
+        </a>
+      ),
+    },
     {
       icon: (
         <a
           href="/JirehTumbagahanCV.pdf"
           download="Jireh_Tumbagahan_Resume.pdf"
-          className="flex items-center justify-center"
         >
           <LuFileDown className="size-5" />
-          &nbsp;
-          <p>Download CV</p>
         </a>
       ),
       fn: () => {},
